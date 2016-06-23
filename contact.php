f7f6b9adfc4ea6be5748e804e2243f9a6cdbf079
BDB88 <abraham.esparza.eb@gmail.com>
2016-06-23 15:58:48 -0500

    esta es la pagina vieja

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+
+$EmailFrom = $_REQUEST['email']; 
+$EmailTo = "ventas@biofuel.mx"; // Your email address here
+$Subject = "Contact form";
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  echo "Error";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $Phone;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= "\n";
+$Body .= "\n";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  echo "Gracias por tu mensaje";
+}
+else{
+  echo "Error";
+}
+?>
